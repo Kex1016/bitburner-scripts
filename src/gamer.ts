ca28a1996f9251daa42ca84f5ff13cd3a469b58b
Kex1016 <cakes@haiiro.moe>
2025-07-29 13:04:19 +0200

    feat(gamer.ts): add a little startup log

--- src/gamer.ts
+++ src/gamer.ts
@@ -1,8 +1,10 @@
 import { NS } from "@ns";
 import { Netwatch } from "./framework/Netwatch";
+import { log } from "./util/log";
 
 export async function main(ns: NS): Promise<void> {
-  const netwatch = new Netwatch(ns);
+  log(ns, "Starting Netwatch...");
 
+  const netwatch = new Netwatch(ns);
   await netwatch.runForever();
 }
